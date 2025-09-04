a3cb21559def511850504ca8035ce0a3fdde67ac
Selsanat <leandro.benites@edu.devinci.fr>
2025-09-04 17:11:58 +0200

    fuck

--- Assets/Scripts/RoadManager.cs
+++ Assets/Scripts/RoadManager.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 using UnityEngine.Splines;
 using NaughtyAttributes;
+using System.Collections.Generic;
+using DG.Tweening;
+using System.Linq;
 
 public class RoadManager : MonoBehaviour
 {
@@ -18,41 +21,91 @@ public class RoadManager : MonoBehaviour
     }
 
     [HideInInspector] public CarController PlayerCar;
+    [HideInInspector] public PoliceController PoliceCar;
     [HideInInspector] public bool WaitForInput = false;
-    [HideInInspector] public Intersection CurrentIntersection;
+    [HideInInspector] public Dictionary<GameObject, Intersection> CurrentIntersections = new Dictionary<GameObject, Intersection>();
     public void MakePlayerFollowSpline(SplineContainer splineContainer)
     {
-        PlayerCar.SplineAnimate.Container = splineContainer;
-        PlayerCar.SplineAnimate.Restart(false);
-        PlayerCar.SplineAnimate.Play();
+        EventManager.instance.onChosingPlayerPath?.Invoke();
+        LerpCarToFirstPoint(PlayerCar.gameObject, splineContainer);
+    }
+
+    public void MakePoliceTakeRandomTurn(GameObject PoliceCar)
+    {
+        Intersection intersection = CurrentIntersections[PoliceCar];
+        // select random turn from available turns
+        List<SplineContainer> availableTurns = new List<SplineContainer>();
+        if (intersection.Up != null) availableTurns.Add(intersection.Up.GetComponent<SplineContainer>());
+        if (intersection.Down != null) availableTurns.Add(intersection.Down.GetComponent<SplineContainer>());
+        if (intersection.Left != null) availableTurns.Add(intersection.Left.GetComponent<SplineContainer>());
+        if (intersection.Right != null) availableTurns.Add(intersection.Right.GetComponent<SplineContainer>());
+        int random = Random.Range(0, availableTurns.Count);
+        SplineContainer randomTurn = availableTurns[random];
+        LerpCarToFirstPoint(PoliceCar, randomTurn);
+        CurrentIntersections[PoliceCar] = null;
+    }
+
+    public void LerpCarToFirstPoint(GameObject car, SplineContainer splineContainer)
+    {
+        Vector3 firstPoint = splineContainer.transform.TransformPoint(splineContainer.Spline.First().Position);
+        car.transform.DOMove(firstPoint, 0.5f).OnComplete(() =>
+        {
+            car.GetComponent<SplineAnimate>().Container = splineContainer;
+            car.GetComponent<SplineAnimate>().Restart(false);
+            car.GetComponent<SplineAnimate>().Play();
+        });
+    }
+    public void DetermineIfReversingIsNeeded(SplineContainer splineContainer, Vector3 carPosition)
+    {
+        float closestDistance = float.MaxValue;
+        int closestKnotIndex = -1;
+        for (int i = 0; i < splineContainer.Spline.Count; i++)
+        {
+            Vector3 knotPosition = splineContainer.transform.TransformPoint(splineContainer.Spline.Knots.ToArray()[i].Position);
+            float distance = Vector3.Distance(carPosition, knotPosition);
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                closestKnotIndex = i;
+            }
+        }
+        if (closestKnotIndex == -1) return;
+        if (closestKnotIndex != 0)
+        {
+            ReverseKnotsOrder(splineContainer);
+        }
+    }
+    public void ReverseKnotsOrder(SplineContainer splineContainer)
+    {
+        splineContainer.ReverseFlow(0);
     }
 
     [Button("Go Up")]
     public void GoUp()
     {
-        MakePlayerFollowSpline(CurrentIntersection.Up.GetComponent<SplineContainer>());
+        MakePlayerFollowSpline(CurrentIntersections[PlayerCar.gameObject].Up.GetComponent<SplineContainer>());
         WaitForInput = false;
-        CurrentIntersection = null;
+        CurrentIntersections[PlayerCar.gameObject] = null;
     }
     [Button("Go Left")]
     public void GoLeft()
     {
-        MakePlayerFollowSpline(CurrentIntersection.Left.GetComponent<SplineContainer>());
+        MakePlayerFollowSpline(CurrentIntersections[PlayerCar.gameObject].Left.GetComponent<SplineContainer>());
         WaitForInput = false;
-        CurrentIntersection = null;
+        CurrentIntersections[PlayerCar.gameObject] = null;
     }
     [Button("Go Right")]
     public void GoRight()
     {
-        MakePlayerFollowSpline(CurrentIntersection.Right.GetComponent<SplineContainer>());
+        MakePlayerFollowSpline(CurrentIntersections[PlayerCar.gameObject].Right.GetComponent<SplineContainer>());
         WaitForInput = false;
-        CurrentIntersection = null;
+        CurrentIntersections[PlayerCar.gameObject] = null;
     }
     [Button("Go Down")]
     public void GoDown()
     {
-        MakePlayerFollowSpline(CurrentIntersection.Down.GetComponent<SplineContainer>());
+        MakePlayerFollowSpline(CurrentIntersections[PlayerCar.gameObject].Down.GetComponent<SplineContainer>());
         WaitForInput = false;
-        CurrentIntersection = null;
+        CurrentIntersections[PlayerCar.gameObject] = null;
     }
 }
